ee158f28831f15d9d41e46c379f91b87b5ae10e2
HalipovEvgeniy <evgeniy.halipov@pagos.ai>
2022-05-19 11:35:14 +0300

    update Cart update Favorite update Catalog fix Search

--- lib/feature/catalog/ui/catalog_screen.dart
+++ lib/feature/catalog/ui/catalog_screen.dart
@@ -3,8 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../constants/constants.dart';
 import '../../../constants/size_config.dart';
-import '../../admin/ui/product_details.dart';
 import '../../cart/ui/widgets/cart_card.dart';
+import '../../product/ui/product_details.dart';
 import '../bloc/catalog_bloc.dart';
 
 class CatalogScreen extends StatelessWidget {
@@ -15,47 +15,49 @@ class CatalogScreen extends StatelessWidget {
     SizeConfig().init(context);
     BlocProvider.of<CatalogBloc>(context).add(FetchCatalog());
     return SafeArea(
-      child: BlocBuilder<CatalogBloc, CatalogState>(
-        builder: (context, state) {
-          if (state is CatalogLoadedState) {
-            return Column(
-              children: [
-                SizedBox(height: getProportionateScreenHeight(20)),
-                Container(
-                  decoration: BoxDecoration(
-                    color: kSecondaryColor.withOpacity(0.1),
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: TextField(
-                    onChanged: (text) {
-                      BlocProvider.of<CatalogBloc>(context)
-                          .add(SearchEvent(text));
-                    },
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.symmetric(
-                        horizontal: getProportionateScreenWidth(20),
-                        vertical: getProportionateScreenWidth(9),
-                      ),
-                      border: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      enabledBorder: InputBorder.none,
-                      hintText: 'Search product',
-                      prefixIcon: const Icon(Icons.search),
-                    ),
-                  ),
+      child: Column(
+        children: [
+          SizedBox(height: getProportionateScreenHeight(20)),
+          Container(
+            decoration: BoxDecoration(
+              color: kSecondaryColor.withOpacity(0.1),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: TextField(
+              onChanged: (text) {
+                BlocProvider.of<CatalogBloc>(context).add(SearchEvent(
+                  text,
+                ));
+              },
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: getProportionateScreenWidth(20),
+                  vertical: getProportionateScreenWidth(9),
                 ),
-                SizedBox(height: getProportionateScreenWidth(10)),
-                Expanded(
+                border: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                enabledBorder: InputBorder.none,
+                hintText: 'Search product',
+                prefixIcon: const Icon(Icons.search),
+              ),
+            ),
+          ),
+          SizedBox(height: getProportionateScreenWidth(10)),
+          BlocBuilder<CatalogBloc, CatalogState>(
+            builder: (context, state) {
+              if (state is CatalogLoadedState) {
+                return Expanded(
                   child: ListView.builder(
                     shrinkWrap: true,
                     itemCount: state.products.length,
                     itemBuilder: (context, index) {
                       final item = state.products[index];
                       return InkWell(
-                        onTap: () => Navigator.pushNamed(
-                          context,
-                          ProductDetails.routeName,
-                          arguments: ProductDetailsArguments(product: item),
+                        onTap: () => showDialog(
+                          useSafeArea: false,
+                          context: context,
+                          builder: (_) =>
+                              ProductDetails(product: state.products[index]),
                         ),
                         child: CartCard(
                           product: item,
@@ -63,18 +65,18 @@ class CatalogScreen extends StatelessWidget {
                       );
                     },
                   ),
-                ),
-              ],
-            );
-          } else {
-            return Container(
-              height: 500,
-              child: const Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
-          }
-        },
+                );
+              } else {
+                return Container(
+                  height: 500,
+                  child: const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              }
+            },
+          ),
+        ],
       ),
     );
   }
